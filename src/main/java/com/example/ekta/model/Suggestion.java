20bf3fbf497a7030a3cbe2c1578f92c9a36470f2
Ravi Raj <ravi.raj@omnicare.com>
2018-09-19 06:40:17 -0400

    testing with model change for mySql

--- src/main/java/com/example/ekta/model/Suggestion.java
+++ src/main/java/com/example/ekta/model/Suggestion.java
@@ -2,12 +2,20 @@ package com.example.ekta.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 
-@Entity(name="SUGGESTION")
-//@Entity /*H2 data base*/
+//@Entity(name="SUGGESTION") /*H2 data base*/
+
+
+@Entity(name="SUGGESTIONS")
 public class Suggestion {
 	
 	@Id
